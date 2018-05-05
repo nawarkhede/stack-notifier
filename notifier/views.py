5c6b8a4055c305791825565dd4cd4387efb6775f
Nishant Nawarkhede <nishant.nawarkhede@gmail.com>
2018-05-05 13:06:49 +0530

    WIP

--- notifier/views.py
+++ notifier/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import StackOverflow
 from bs4 import BeautifulSoup
 import urllib2
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    stacks = StackOverflow.objects.order_by('-question_id')
+    return render(request, 'index.html', {'stacks': stacks})
 
 
 
